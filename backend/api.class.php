6558c6e6922c485446679b032a15153754b7c85c
Yusuf Aktan <57505988+yusufaktan@users.noreply.github.com>
2023-10-23 12:01:08 +0300

    Update api.class.php

--- backend/api.class.php
+++ backend/api.class.php
@@ -2,7 +2,7 @@
     header("Access-Control-Allow-Origin: *");  // Tüm kaynaklardan erişime izin verir
     header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
     header("Access-Control-Allow-Methods: GET, POST");  // Sadece GET ve POST metodlarına izin verir
-    class api{
+    class api {
         private $db;
         private $dbUsername;
         private $dbPassword;
@@ -11,32 +11,33 @@
         private $username;
         private $password;
         private $config;
-        private $token;
-
+    
         public function __construct() {
-            
-                global $config;
-                $this->config       = $config;
-                $this->dbHost       = $config["dbHost"];
-                $this->dbUsername   = $config["dbUsername"];
-                $this->dbPassword   = $config["dbPassword"];
-                $this->dbName       = $config["dbName"];
-                $this->username     = $config["username"];
-                $this->password     = $config["password"];
-                $this->db = new MysqliDb($this->dbHost, $this->dbUsername, $this->dbPassword, $this->dbName );
-                if(!$this->db){
-                    $retData = $this->errorCreator("601",false);
-                }else{
-                    $retData["status"]  = true; 
-                }
-                return $retData;
-            
+            global $config;
+            $this->config = $config;
+            $this->dbHost = $config["dbHost"];
+            $this->dbUsername = $config["dbUsername"];
+            $this->dbPassword = $config["dbPassword"];
+            $this->dbName = $config["dbName"];
+            $this->username = $config["username"];
+            $this->password = $config["password"];
+            $this->db = new MysqliDb($this->dbHost, $this->dbUsername, $this->dbPassword, $this->dbName);
+            if (!$this->db) {
+                $retData = $this->errorCreator("601", false);
+            } else {
+                $retData["status"] = true;
+            }
+            return $retData;
         }
-
+    
         public function login($username, $password) {
+            session_unset();
+            session_destroy();
+            session_start();
             $user = $this->db->where('username', $username)
                              ->where('password', $password)
                              ->getOne('students');
+    
             if ($user) {
                 $retData["student_id"] = $user['student_id'];
                 $retData["token"] = $this->createToken();
@@ -44,71 +45,84 @@
             } else {
                 $retData = $this->errorCreator("101", false);
             }
-        
+    
             return (object) $retData;
         }
-        
-        public function errorCreator($code,$status){
-                $retData["description"] = $this->config["error"][$code];
-                $retData["code"]        = $code;
-                $retData["status"]      = $status;
-                return $retData;
+    
+        public function errorCreator($code, $status) {
+            $retData["description"] = $this->config["error"][$code];
+            $retData["code"] = $code;
+            $retData["status"] = $status;
+            return $retData;
         }
-
-        public function createToken(){
-                $currentTime = time();
-                if(!isset($_SESSION["token"]) || !isset($_SESSION["token_creation_time"]) || ($currentTime - $_SESSION["token_creation_time"] >= 1800)){
-                    $token =$this->generateRandomString(16);
-                    $this->token = $token;
-                    $_SESSION["token"] = $this->token;
-                    $_SESSION["token_creation_time"] = $currentTime;
-                }
-                return $_SESSION["token"];
+    
+        private function isValidToken() {
+            $currentTime = time();
+            if (!isset($_SESSION["token"]) || !isset($_SESSION["token_creation_time"]) || ($currentTime - $_SESSION["token_creation_time"] >= 1800)) {
+                return false;
+            }
+            return true;
         }
-
+    
+        public function createToken() {
+            $currentTime = time();
+            $token = $this->generateRandomString(16);
+            $_SESSION["token"] = $token;
+            $_SESSION["token_creation_time"] = $currentTime;
+            return $token;
+        }
+    
         public function generateRandomString($length = 10) {
             $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
             $charactersLength = strlen($characters);
             $randomString = '';
             for ($i = 0; $i < $length; $i++) {
-                $randomString .= $characters[rand(0,62)];
+                $randomString .= $characters[rand(0, 61)];
             }
             return $randomString;
         }
-
-        public function getBody($body){
-                $body_arr = json_decode($body, true);
-                if ($body_arr) {
-                    $retData=$body_arr;
-                    $retData["status"] = true;
-                }else{
-                    $retData = $this->errorCreator("102",false);
-                }
-                return (object)$retData;
+    
+        public function getBody($body) {
+            $body_arr = json_decode($body, true);
+            if ($body_arr) {
+                $retData = $body_arr;
+                $retData["status"] = true;
+            } else {
+                $retData = $this->errorCreator("102", false);
+            }
+            return (object) $retData;
         }
-
+    
         public function getAllCourses() {
+            if (!$this->isValidToken()) {
+                return $this->errorCreator("107", false);
+            }
+    
             $courses = $this->db->get('courses');
-            
+    
             if ($this->db->count > 0) {
                 $retData["courses"] = $courses;
                 $retData["status"] = true;
             } else {
                 $retData = $this->errorCreator("102", false);
             }
-            
+    
             return (object) $retData;
         }
-
+    
         public function getStudentCourses($studentId) {
+            if (!$this->isValidToken()) {
+                return $this->errorCreator("107", false);
+            }
+    
             if (!is_numeric($studentId)) {
                 $retData = $this->errorCreator("102", false);
                 return (object) $retData;
             }
-        
+    
             $this->db->where('student_id', $studentId);
             $enrollments = $this->db->get('student_courses');
-        
+    
             if ($this->db->count > 0) {
                 $courses = array();
                 foreach ($enrollments as $enrollment) {
@@ -120,25 +134,29 @@
             } else {
                 $retData = $this->errorCreator("102", false);
             }
-            
+    
             return (object) $retData;
         }
-
+    
         public function enrollStudentInCourse($studentId, $courseId) {
+            if (!$this->isValidToken()) {
+                return $this->errorCreator("107", false);
+            }
+    
             $existingEnrollment = $this->db->where('student_id', $studentId)
                                            ->where('course_id', $courseId)
                                            ->getOne('student_courses');
-        
+    
             if ($existingEnrollment) {
-                $retData = $this->errorCreator("103", false); 
+                $retData = $this->errorCreator("103", false);
             } else {
                 $enrollmentData = array(
                     'student_id' => $studentId,
                     'course_id' => $courseId
                 );
-        
+    
                 $enrollmentId = $this->db->insert('student_courses', $enrollmentData);
-        
+    
                 if ($enrollmentId) {
                     $retData["status"] = true;
                     $retData["message"] = "Öğrenci kursa başarıyla kaydedildi.";
@@ -146,20 +164,24 @@
                     $retData = $this->errorCreator("104", false);
                 }
             }
-        
+    
             return (object) $retData;
         }
-
+    
         public function removeStudentFromCourse($studentId, $courseId) {
+            if (!$this->isValidToken()) {
+                return $this->errorCreator("107", false);
+            }
+    
             $existingEnrollment = $this->db->where('student_id', $studentId)
                                            ->where('course_id', $courseId)
                                            ->getOne('student_courses');
-        
+    
             if ($existingEnrollment) {
                 $result = $this->db->where('student_id', $studentId)
                                   ->where('course_id', $courseId)
                                   ->delete('student_courses');
-        
+    
                 if ($result) {
                     $retData["status"] = true;
                     $retData["message"] = "Öğrenci kursdan başarıyla çıkarıldı.";
@@ -169,11 +191,10 @@
             } else {
                 $retData = $this->errorCreator("106", false);
             }
-        
+    
             return (object) $retData;
         }
-        
-        
     }
+
 ?>
 
